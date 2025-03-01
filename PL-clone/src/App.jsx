7b4ac28b6b7df9d50da40370288ae45355bdba0e
Govind Sharma <govind01sharma@yahoo.in>
2025-03-01 13:05:27 +0530

    Logo applied

--- PL-clone/src/App.jsx
+++ PL-clone/src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header'
+import HomePage from './pages/HomePage'
 function App() {
 
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Header />
-        
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   )
--- PL-clone/src/components/Header.jsx
+++ PL-clone/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/images/logo.png'
+
 function Header() {
   return (
     <img src={logo} alt="logo" className="w-[80px] md:w-[115px] object-cover" />
