f12db57af66b90a9732f96d2eae208df8158a238
Govind Sharma <govind01sharma@yahoo.in>
2025-03-04 09:59:59 +0530

    Remove unused Router import from HomePage component

--- PL-clone/src/pages/HomePage.jsx
+++ PL-clone/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Fixtures from '../components/Fixtures';
 import NewsSection from '../components/NewsSection';
 import ClubNews from '../components/ClubNews';
