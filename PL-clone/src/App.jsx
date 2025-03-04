6946e73ba51e3bb970f5b6d0a267027263950c5d
Govind Sharma <govind01sharma@yahoo.in>
2025-03-04 10:59:36 +0530

    Add Fixtures route to App component for match display

--- PL-clone/src/App.jsx
+++ PL-clone/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import Fixtures from "./components/Fixtures";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <main className="flex-grow w-full pt-[200px]">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/fixtures" element={<Fixtures />} />
           </Routes>
         </main>
         <Footer /> 
