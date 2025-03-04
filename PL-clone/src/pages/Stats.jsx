a10097ec2c2a62209d0d8f26d5e87f4f2eb01dec
Govind Sharma <govind01sharma@yahoo.in>
2025-03-04 13:12:35 +0530

    Minor UI change

--- PL-clone/src/pages/Stats.jsx
+++ PL-clone/src/pages/Stats.jsx
@@ -73,7 +73,7 @@ const statsData = [
 const Stats = () => {
   return (
     <div className="container mx-auto px-2 py-10">
-      <h1 className="text-2xl font-semibold mb-5">2024/25 Player Stats</h1>
+      <h1 className="text-[#37003C] text-4xl font-bold mb-5 custom-font-family">2024/25 Player Stats</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {statsData.map((stat, index) => (
           <div
